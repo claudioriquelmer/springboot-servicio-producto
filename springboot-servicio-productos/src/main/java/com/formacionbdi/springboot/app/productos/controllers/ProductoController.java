45c343ad4184040e9ff6223e5dd9c531276aaf6f
claudioriquelmer <claudio.riquelme@outlook.es>
2024-06-10 16:36:19 -0400

    Probando Resilience4J con los criterios por defecto

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -1,6 +1,7 @@
 package com.formacionbdi.springboot.app.productos.controllers;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,14 +36,15 @@ public class ProductoController {
 	}
 
 	@GetMapping("/ver/{id}")
-	public Producto detalle(@PathVariable Long id) {
+	public Producto detalle(@PathVariable Long id) throws InterruptedException {
+		if (id.equals(10L)) {
+			throw new IllegalStateException("Producto no encontrado!");
+		}
+		if (id.equals(7L)) {
+			TimeUnit.SECONDS.sleep(5L);
+		}
 		Producto producto = productoService.findById(id);
 		producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-
-		
-		 try { Thread.sleep(2000L); } catch (InterruptedException e) {
-		 e.printStackTrace(); }
-		 
 
 		return producto;
 	}
